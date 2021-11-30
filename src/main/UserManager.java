257ee90e53f0cdb793565b67992593be87e85b94
AryanJain107 <81820070+AryanJain107@users.noreply.github.com>
2021-11-30 15:45:12 -0500

    Synchronized UserManager File

--- src/main/UserManager.java
+++ src/main/UserManager.java
@@ -1,9 +1,10 @@
+//TODO: Gives reasons for why each part of the code is synchronized
 package main;
+
 import java.util.ArrayList;
 import java.util.Random;
 
 /**
- *
  * The UserManager of the application
  * <p>
  * Implements the Manager class
@@ -13,128 +14,144 @@ import java.util.Random;
  *
  * @author Aryan Jain
  * @version 1.0.0
- *
  */
 
 public class UserManager implements Manager {
+    //Created an Object named obj to synchronize elements of this code
+    Object obj = new Object();
+    //Arraylist called users that has user objects
+    private ArrayList<User> users = new ArrayList<>();
 
-	//Arraylist called users that has user objects
-	private ArrayList<User> users = new ArrayList<>();
+    LearningManagementSystem lms;
 
-	LearningManagementSystem lms;
-	/**
-	 * Constructor for the UserManager.
-	 * <p>
-	 * All initialization is done in {@link #init()}, so the
-	 * constructor solely sets the {@link #lms} field.
-	 */
-	public UserManager(LearningManagementSystem lms) {
-		this.lms = lms;
-	}
+    /**
+     * Constructor for the UserManager.
+     * <p>
+     * All initialization is done in {@link #init()}, so the
+     * constructor solely sets the {@link #lms} field.
+     */
+    public UserManager(LearningManagementSystem lms) {
+        this.lms = lms;
+    }
 
-	/**
-	 * Returns an ArrayList users of User objects
-	 */
-	public ArrayList<User> getUsers() {
-		return users;
-	}
+    /**
+     * Returns an ArrayList users of User objects
+     */
+    public ArrayList<User> getUsers() {
 
-	/**
-	 * Set an ArrayList users of User objects
-	 */
-	public void setUsers(ArrayList<User> users) {
-		this.users = users;
-	}
+        synchronized (obj) {
+            return users;
+        }
+    }
 
-	/**
-	 * Adds a User object to the ArrayList users
-	 */
-	public void addUser(User user) {
-		users.add(user);
-	}
+    /**
+     * Set an ArrayList users of User objects
+     */
+    public void setUsers(ArrayList<User> users) {
+        this.users = users;
+    }
 
-	/**
-	 * Removes a User object from the ArrayList users
-	 */
-	public void removeUser(User user) { users.remove(user); }
+    /**
+     * Adds a User object to the ArrayList users
+     */
+    public void addUser(User user) {
+        synchronized (obj) {
+            users.add(user);
+        }
+    }
 
-	/**
-	 * Takes an username parameter and a password parameter
-	 * and returns boolean if a specific user's
-	 * username matches its password
-	 */
-	public boolean authenticator(String username, String password) {
-		for (User user : users) {
-			if ((user.getUsername().equals(username)) && (user.getPassword().equals(password))) {
-				return true;
-			}
-		}
-		return false;
-	}
+    /**
+     * Removes a User object from the ArrayList users
+     */
+    public void removeUser(User user) {
+        synchronized (obj) {
+            users.remove(user);
+        }
+    }
 
-	/**
-	 * Iterates through the ArrayList users and returns a user based
-	 * on a search by username
-	 */
-	public User getUser(String username) {
-		for (User user : users) {
-			if (user.getUsername().equals(username)) {
-				return user;
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Iterates through the ArrayList users and returns a user based
-	 * on a search by User ID
-	 */
-	public User getUserById(int id) {
-		for (User user : users) {
-			if (user.getID() == id) {
-				return user;
-			}
-		}
-		return null;
-	}
-	
-	/**
-	 * Generates a uniqueID for each User object in the ArrayList users
-	 */
-	public int getUniqueID() {
-		Random rand = new Random();
-		int id = 0;
-		boolean exists = true;
-		while (exists) {
-			exists = false;
-			id = rand.nextInt(999999);
-			for (int i = 0; i < users.size(); i++) {
-				if (users.get(i).getID() == i) {
-					exists = true;
-				}
-			}
-		}
-		return id;
-	}
-
-	/**
-	 * Default init method from the Manager Interface
-	 * Is blank because this method is not necessary for this class
-	 */
-	@Override
-	public void init() {
-
-	}
-
-	/**
-	 * Default exit method from the Manager Interface
-	 * Is blank because this method is not necessary for this class
-	 */
-	@Override
-	public void exit() {
-
-	}
+    /**
+     * Takes an username parameter and a password parameter
+     * and returns boolean if a specific user's
+     * username matches its password
+     */
+    public boolean authenticator(String username, String password) {
+        for (User user : users) {
+            synchronized (obj) {
+                if ((user.getUsername().equals(username)) && (user.getPassword().equals(password))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 
+    /**
+     * Iterates through the ArrayList users and returns a user based
+     * on a search by username
+     */
+    public User getUser(String username) {
+        for (User user : users) {
+            synchronized (obj) {
+                if (user.getUsername().equals(username)) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
 
+    /**
+     * Iterates through the ArrayList users and returns a user based
+     * on a search by User ID
+     */
+    public User getUserById(int id) {
+        for (User user : users) {
+            synchronized (obj) {
+                if (user.getID() == id) {
+                    return user;
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Generates a uniqueID for each User object in the ArrayList users
+     */
+    public int getUniqueID() {
+        Random rand = new Random();
+        int id = 0;
+        boolean exists = true;
+        while (exists) {
+            exists = false;
+            id = rand.nextInt(999999);
+            for (int i = 0; i < users.size(); i++) {
+                synchronized (obj) {
+                    if (users.get(i).getID() == i) {
+                        exists = true;
+                    }
+                }
+            }
+        }
+        return id;
+    }
+
+    /**
+     * Default init method from the Manager Interface
+     * Is blank because this method is not necessary for this class
+     */
+    @Override
+    public void init() {
+
+    }
+
+    /**
+     * Default exit method from the Manager Interface
+     * Is blank because this method is not necessary for this class
+     */
+    @Override
+    public void exit() {
+
+    }
 }
