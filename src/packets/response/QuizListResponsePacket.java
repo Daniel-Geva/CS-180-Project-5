bbf320d88cdc96550ee6056555189407c58d3e67
liamkelly <kellylj@purdue.edu>
2021-11-30 22:31:46 -0500

    Worked on both Quiz Response PAckets

--- src/packets/response/QuizListResponsePacket.java
+++ src/packets/response/QuizListResponsePacket.java
@@ -1,4 +1,38 @@
 package packets.response;
 
-public class QuizListResponsePacket {
+import server.LearningManagementSystemServer;
+
+import java.util.ArrayList;
+
+public class QuizListResponsePacket extends ResponsePacket {
+    LearningManagementSystemServer lms;
+    ArrayList<Quiz> quizzesToReturn = new ArrayList<Quiz>();
+    String searchTerm;
+    public QuizListResponsePacket(LearningManagementSystemServer lms,  String requestType, String searchTerm) {
+        this.lms = lms;
+        this.searchTerm = searchTerm;
+        switch (requestType) {
+            case "All":
+                quizzesToReturn = lms.getQuizManager().getQuizList();
+                break;
+            case "Author":
+                quizzesToReturn = lms.getQuizManager().searchQuizByAuthor(searchTerm);
+                break;
+            case "Name":
+                quizzesToReturn = lms.getQuizManager().searchQuizByName(searchTerm);
+                break;
+            case "Course":
+                quizzesToReturn = lms.getQuizManager().searchQuizByCourse(searchTerm);
+                break;
+            default:
+                quizzesToReturn = null;
+        }
+    }
+
+    public ArrayList<Quiz> getListOfQuizzesResponse() {
+        return quizzesToReturn;
+    }
+
+
+
 }
--- src/packets/response/QuizResponsePacket.java
+++ src/packets/response/QuizResponsePacket.java
@@ -1,4 +1,18 @@
 package packets.response;
 
-public class QuizResponsePacket {
+import server.LearningManagementSystemServer;
+
+public class QuizResponsePacket extends ResponsePacket{
+    LearningManagementSystemServer lms;
+    Quiz quizToReturn;
+
+    public QuizResponsePacket(LearningManagementSystemServer lms, int id) {
+        this.lms = lms;
+        quizToReturn = lms.getQuizManager().searchQuizByID(id);
+    }
+
+    public Quiz getQuizResponse() {
+        return quizToReturn;
+    }
+
 }
