b49ccef5f9f1eec448dac383fb1a6bcd0647b830
seeenlee <naeseel18@gmail.com>
2021-12-07 15:50:59 -0500

    added Javadoc to GradedQuizRequestPacket

--- src/packets/request/GradedQuizRequestPacket.java
+++ src/packets/request/GradedQuizRequestPacket.java
@@ -1,12 +1,18 @@
 package packets.request;
-
-import packets.response.QuizResponsePacket;
-import packets.response.ResponsePacket;
 import server.LearningManagementSystemServer;
-
 import java.io.Serializable;
 import packets.response.GradedQuizResponsePacket;
 
+/**
+ * Graded Quiz Request Packet
+ *
+ * Contains the method that handles a handle request by the server.
+ *
+ * @author Sean Lee
+ * @version 12/7/21
+ * @see GradedQuizResponsePacket
+ * @see datastructures.GradedQuiz
+ */
 public class GradedQuizRequestPacket implements Serializable {
     int id;
 
