c76872f166f3388e1ccc115725a471816a773d1b
Isaac Fleetwood <ifleetwo@purdue.edu>
2021-12-07 17:01:33 -0500

    Add

--- src/datastructures/PushPacketHandler.java
+++ src/datastructures/PushPacketHandler.java
@@ -1,17 +1,12 @@
 package datastructures;
 
 import client.ResponsePacketHandler;
-import packets.push.PushPacket;
 import packets.response.ResponsePacket;
 
-public abstract class PushPacketHandler<T extends PushPacket> extends ResponsePacketHandler {
+public abstract class PushPacketHandler<T extends ResponsePacket> extends ResponsePacketHandler {
 
-	public boolean canHandle(PushPacket packet) {
+	public boolean canHandle(ResponsePacket packet) {
 		return (packet instanceof T);
-	}
-	
-	public void run(ResponsePacket responsePacket) {
-		
 	}
 	
 	public abstract void run(T packet);
