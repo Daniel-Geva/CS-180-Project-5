1cbabd82587d567dac1c99e0a23fdbddd9bb2a86
seeenlee <naeseel18@gmail.com>
2021-12-10 14:52:05 -0500

    removed success from ResponsePacket and added GradedQuizListRequestPacket

--- src/packets/response/ResponsePacket.java
+++ src/packets/response/ResponsePacket.java
@@ -6,26 +6,30 @@ public class ResponsePacket implements Serializable {
 
     private static final long serialVersionUID = -5153672930902069453L;
 
-    boolean success;
+//    boolean success;
 
     public ResponsePacket() {
-        this.success = true;
+//        this.success = true;
         this.push = false;
     }
 
-    public ResponsePacket(boolean success) {
-        this.success = success;
-        this.push = false;
-    }
+//    public ResponsePacket(boolean success) {
+//        this.success = success;
+//        this.push = false;
+//    }
 
-    public ResponsePacket(boolean success, boolean push) {
-        this.success = success;
+//    public ResponsePacket(boolean success, boolean push) {
+//        this.success = success;
+//        this.push = push;
+//    }
+
+    public ResponsePacket(boolean push) {
         this.push = push;
     }
 
-    public boolean wasSuccess() {
-        return this.success;
-    }
+//    public boolean wasSuccess() {
+//        return this.success;
+//    }
 
     private boolean push;
 
