bb5b9073f27a1956baddd469d8a0eb71711fed62
Luderius <dxgeva@gmail.com>
2021-12-11 15:18:06 -0500

    javadoc comments

--- src/client/RunnableHandleResponsePacket.java
+++ src/client/RunnableHandleResponsePacket.java
@@ -1,7 +1,16 @@
 package client;
 
 import packets.response.ResponsePacket;
-
+/**
+ * short description
+ * <p>
+ * functionality
+ *
+ * @author Isaac Fleetwood
+ *
+ * @version December 10, 2021
+ *
+ */
 public interface RunnableHandleResponsePacket {
     public void handlePacket(ResponsePacket responsePacket);
 }
