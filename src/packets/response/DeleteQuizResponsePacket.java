57d376c9bd2382b123c99ff8d344fb9e758c5112
liamkelly <kellylj@purdue.edu>
2021-12-12 13:15:59 -0500

    Added push to delete quiz

--- src/packets/response/DeleteQuizResponsePacket.java
+++ src/packets/response/DeleteQuizResponsePacket.java
@@ -5,6 +5,7 @@ import packets.request.DeleteQuizRequestPacket;
 public class DeleteQuizResponsePacket extends ResponsePacket{
     boolean success;
     public DeleteQuizResponsePacket(boolean success) {
+        super(success, true);
         this.success = success;
     }
 
