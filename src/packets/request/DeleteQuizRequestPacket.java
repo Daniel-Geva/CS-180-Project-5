17c14f8ee34de337d84ba9776052298aff7c5275
liamkelly <kellylj@purdue.edu>
2021-12-12 11:52:41 -0500

    Made it so graded quizzes matching a quiz id are also removed when the quiz is removed

--- src/packets/request/DeleteQuizRequestPacket.java
+++ src/packets/request/DeleteQuizRequestPacket.java
@@ -5,6 +5,8 @@ import packets.response.DeleteQuizResponsePacket;
 import packets.response.ResponsePacket;
 import server.LearningManagementSystemServer;
 
+import java.util.ArrayList;
+
 public class DeleteQuizRequestPacket extends RequestPacket{
     int id;
 
@@ -16,6 +18,13 @@ public class DeleteQuizRequestPacket extends RequestPacket{
     public ResponsePacket serverHandle(LearningManagementSystemServer lms) {
         if (lms.getQuizManager().searchQuizByID(id) != null) {
             lms.getQuizManager().removeQuiz(id);
+            ArrayList<GradedQuiz> gradedQuizList = lms.getGradedQuizManager().getGradedQuizList();
+            for (int i = 0; i < gradedQuizList.size(); i++) {
+                if (id == gradedQuizList.get(i).getQuizID()) {
+                    gradedQuizList.remove(i);
+                }
+            }
+            lms.getGradedQuizManager().setGradedQuiz(gradedQuizList);
             return new DeleteQuizResponsePacket(true);
         } else {
             return new DeleteQuizResponsePacket(false);
