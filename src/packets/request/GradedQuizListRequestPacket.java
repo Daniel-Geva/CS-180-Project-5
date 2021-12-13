d263ea9b5fcd81931352c0b6365b922e2d2ff186
seeenlee <naeseel18@gmail.com>
2021-12-13 13:49:47 -0500

    updated GradedQuizManager

--- src/packets/request/GradedQuizListRequestPacket.java
+++ src/packets/request/GradedQuizListRequestPacket.java
@@ -9,6 +9,12 @@ import packets.response.GradedQuizListResponsePacket;
 import packets.response.ResponsePacket;
 import server.LearningManagementSystemServer;
 
+/**
+ * Request packet for a list of graded quizzes
+ *
+ * @author Issac Fleetwood
+ * @version December 9, 2021
+ */
 public class GradedQuizListRequestPacket extends RequestPacket {
 	
 	User user;
@@ -24,7 +30,7 @@ public class GradedQuizListRequestPacket extends RequestPacket {
 	@Override
 	public ResponsePacket serverHandle(LearningManagementSystemServer lms) {
 		List<GradedQuiz> list = new ArrayList<>();
-		if(user == null)
+		if (user == null)
 			list = lms.getGradedQuizManager().getGradedQuizList();
 		else 
 			list = lms
