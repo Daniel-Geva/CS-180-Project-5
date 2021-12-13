f3c4c5ed237771057da66bc2e2f5eb75008db46f
Isaac Fleetwood <ifleetwo@purdue.edu>
2021-12-12 23:33:45 -0500

    Add Run_Voccheck

--- run_voccheck.sh
+++ run_voccheck.sh
@@ -1,6 +1,10 @@
 # Copy all java files to root directory, so that voccheck can check them
-cp ./src/main/*.java .
-cp ./src/ui/*.java .
+cp ./src/client/*.java .
+cp ./src/datastructures/*.java .
+cp ./src/gui/*.java .
+cp ./src/packets/response/*.java .
+cp ./src/packets/request/*.java .
+cp ./src/server/*.java .
 
 # Run voccheck, and pipe the result to voccheck_log
 voccheck 2>&1 | tee voccheck_log
