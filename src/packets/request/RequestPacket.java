38852e6c8bf681ed64b63f72c8158e5b8dca5552
Luderius <dxgeva@gmail.com>
2021-12-13 13:37:09 -0500

    Fixed voccheck errors

--- src/packets/request/RequestPacket.java
+++ src/packets/request/RequestPacket.java
@@ -5,8 +5,21 @@ import java.io.Serializable;
 import packets.response.ResponsePacket;
 import server.LearningManagementSystemServer;
 
+/**
+ * Parent class for all RequestPacket types
+ * <p>
+ * Sent from the client to the server to ask for information or to notify of a change
+ *
+ * @author Isaac Fleetwood
+ */
 public abstract class RequestPacket implements Serializable {
 
+    /**
+     * Method called in order to 'handle' or unpack the instructions of the packet
+     *
+     * @param lms - The server manager is provided in order to insure access to necessary information
+     * @return responsePacket - Packet sent back to the client containing the server's response
+     */
     public abstract ResponsePacket serverHandle(LearningManagementSystemServer lms);
     
 }
