6e20fb8cbd2646a22e6a1c6fe5569d107dc90f7f
Isaac Fleetwood <ifleetwo@purdue.edu>
2021-12-13 15:25:16 -0500

    Add Javadocs

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"main"},{"l":"ui"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"client"},{"l":"datastructures"},{"l":"gui"},{"l":"packets.request"},{"l":"packets.response"},{"l":"server"}];updateSearchResults();
